f3fb20423fc8e6933325cb255eb6c4b9986b6fdf
Jack Bryant <jackbryant1098@hotmail.com>
2020-02-23 17:40:51 -0700

    Login and Account Creation Design (no functions yet...)

---
+++ Capstone Mock/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  Capstone Mock
+//
+//  Created by JACK BRYANT on 2/23/20.
+//  Copyright © 2020 Jack Bryant. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var userEmail: UITextField!
+    
+    @IBOutlet weak var userPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
