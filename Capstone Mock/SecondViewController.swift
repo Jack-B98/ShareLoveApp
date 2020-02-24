c4636a37b4ca4efbb39842f8804cdd8b96ee78a4
Brandon Valdivia <branvaldivia@gmail.com>
2020-02-23 17:46:41 -0700

    Share Love Tab Created
    Added in the share love tab and icon. Page has a functional number pad and display along with backspace. When the user selects to send their payment, it will display a message that will tell them the payment was sent and how much was sent. The number entered label will then be cleared.

--- Capstone Mock/SecondViewController.swift
+++ Capstone Mock/SecondViewController.swift
@@ -1,20 +1,50 @@
 //
 //  SecondViewController.swift
-//  Capstone Mock
+//  capstoneProject
 //
-//  Created by JACK BRYANT on 2/23/20.
-//  Copyright © 2020 Jack Bryant. All rights reserved.
+//  Created by btvaldiv on 2/23/20.
+//  Copyright © 2020 btvaldiv. All rights reserved.
 //
 
 import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func numbers(_ sender: UIButton) {
+        totalLabel.text = totalLabel.text! + String(sender.tag - 1)
+        
+    }
+    @IBAction func backspace(_ sender: UIButton) {
+        if(totalLabel.text!.count > 1){
+            totalLabel.text?.removeLast()
+        }
+    }
+    
+    @IBAction func shareTheLove(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Your Love's Been Shared", message: "Thank you so much for sharing the love in the world. Your donation of "+totalLabel.text!+" was recieved by Random Selected User Name Here", preferredStyle: .alert)
+        
+        
+        
+        
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            
+            
+            self.totalLabel.text = "$"
+    
+            
+        }))
+        
+        
+        
+        self.present(alert, animated: true)
+        
+    }
 }
 
